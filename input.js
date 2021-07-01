b91311c6ba7e1cb7c187fdf5f7743ea25c8492d1
vagrant <chiomaanierobi@gmail.com>
2021-07-01 04:40:52 +0000

    WASD movement implemted

--- input.js
+++ input.js
@@ -1,21 +1,44 @@
 // setup interface to handle user input from stdin
-const setupInput = function(conn) {
+
+let connection; //   => Stores the active TCP connection object.
+const upKey = 'w';
+const leftKey = 'a';
+const downKey = 's';
+const rightKey = 'd';
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
-    }
-    // else if( key === 'a') {
-    //   conn.write("Move: left")
-    // }
-  };
 
+    } else if (key === upKey) {
+      conn.write("Move: up");
+    
+    } else if (key === leftKey) {
+      conn.write("Move: left");
+
+    } else if (key === downKey) {
+      conn.write("Move: down");
+
+    } else if (key === rightKey) {
+      conn.write("Move: right");
+    }
+
+  };
   stdin.on("data", handleUserInput);
   return stdin;
+    
 };
+
+ 
+
 
 module.exports = { setupInput };
